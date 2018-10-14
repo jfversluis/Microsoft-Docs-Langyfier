009ea2ddceb766f6ecbfa360a838aeb022ec43b7
Gerald Versluis <gerald@verslu.is>
2018-10-14 21:08:52 +0200

    Initial commit

---
+++ app/background.js
@@ -0,0 +1,32 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function () {
+    localStorage["enableExtension"] = "true";
+
+    chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
+        if (localStorage["enableExtension"] != "true")
+            return;
+
+        if (localStorage["unwantedLanguage"] === "" || localStorage["preferedLanguage"] === "")
+            return;
+
+        if (details.url.indexOf("docs.microsoft.com") != -1)
+        {
+            if (details.url.indexOf(localStorage["unwantedLanguage"]) != -1)
+            {
+                chrome.tabs.update(details.tabId, {
+                    url: details.url.replace(new RegExp(localStorage["unwantedLanguage"], "gi"), localStorage["preferedLanguage"]) });
+            }
+        }       
+    })
+});
+
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([{
+        conditions: [new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'docs.microsoft.com' },
+        })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+});
---
+++ app/settings.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    $("#enableExtensionCheckbox").prop("checked", localStorage["enableExtension"] === "true");
+    $("#unwantedLanguagePicker").val(localStorage["unwantedLanguage"]);
+    $("#preferedLanguagePicker").val(localStorage["preferedLanguage"]);
+
+    updateFormEnabled($("#enableExtensionCheckbox")[0].checked);
+
+    // Handle enable/disable checkbox
+    $('#enableExtensionCheckbox').on('change', function (e) {
+        var enabled = $("#enableExtensionCheckbox")[0].checked;
+        localStorage["enableExtension"] = enabled;
+
+        updateFormEnabled(enabled);
+    });
+
+    // Handle save button click
+    $("#saveButton").click(function () {
+        try {
+            var unwanted = $('#unwantedLanguagePicker').val();
+            var prefered = $('#preferedLanguagePicker').val();
+
+            if (unwanted === "" || prefered === "") {
+                $("#success-warning").fadeTo(2000, 500).slideUp(500, function () {
+                    $("#success-warning").slideUp(500);
+                });
+                
+                return;
+            }
+
+            localStorage["unwantedLanguage"] = unwanted;
+            localStorage["preferedLanguage"] = prefered;
+
+            $("#success-alert").fadeTo(2000, 500).slideUp(500, function () {
+                $("#success-alert").slideUp(500);
+            });
+        }
+        catch (exception)
+        {
+            $("#success-error").fadeTo(2000, 500).slideUp(500, function () {
+                $("#success-error").slideUp(500);
+            });
+        }
+    });
+});
+
+function updateFormEnabled(enabled) {
+    $("#preferedLanguagePicker").prop('disabled', !enabled);
+    $("#unwantedLanguagePicker").prop('disabled', !enabled);
+}
